d022897b97bd6959a819d5bdb57bce25e7fbf4e3
yasa685 <yasa685@HP-CND3061S12.localdomain>
2026-09-24 18:38:23 +0000

    commited_on_9_25

---
+++ stop_ec2_instance.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+############################################
+# Author : SAI YADAV
+# Date   : 8/24/2026
+# Purpose: Stop AWS EC2 instance
+############################################
+
+set -e
+
+source ./aws_credentials.env
+
+echo "Stopping EC2 instance..."
+
+aws configure set aws_access_key_id "$ACCESS_KEY"
+aws configure set aws_secret_access_key "$SECRET_KEY"
+aws configure set region "ap-south-1"
+aws configure set output "json"
+
+aws ec2 stop-instances \
+    --instance-ids "$INSTANCE_ID" \
+    --region ap-south-1
+
+echo "EC2 instance stop command sent successfully."
+
+#END 
